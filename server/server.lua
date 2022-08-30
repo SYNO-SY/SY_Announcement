d0bdc1685748b8a2687107c12f2c5fe766824d11
SYNO <89760730+SYNO-Dev@users.noreply.github.com>
2022-08-30 09:40:23 +0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -9,18 +9,15 @@ function strikethrough(text) return "<del>"..text.."</del>" end
 function italic(text) return "<i>"..text.."</i>" end
 function big(text) return "<big>"..text.."</big>" end
 
-RegisterCommand("ann", function(source, args)
+RegisterCommand("annu", function(source, args)
     local argString = table.concat(args, " ")
     if argString ~= nil then
-        TriggerClientEvent('syno_announce:display', -1, '[T L R P]',  argString, 10000)
+        TriggerClientEvent('sy_announce:display', -1, 'ANNOUNCEMENT',  argString, 10000)
     end
 end, true)
 
 Citizen.CreateThread(function()
-	if (GetCurrentResourceName() ~= "syno_announcement") then 
-		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named syno_announcement for it to work properly!");
-		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named syno_announcement for it to work properly!");
-		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named syno_announcement for it to work properly!");
-		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named syno_announcement for it to work properly!");
+	if (GetCurrentResourceName() ~= "SY_Announcement") then 
+		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named SY_Announcement for it to work properly!");
 	end
 end)
